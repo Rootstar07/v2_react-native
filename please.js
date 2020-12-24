865b0da58a8db7eae6324c64738929cb42435112
GeunTae KIM <60951051+Rootstar07@users.noreply.github.com>
2020-12-24 21:56:42 +0900

    11
    구축끝

---
+++ please.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function please() {
+    return (
+    <View>
+        <Text>성공!</Text>
+    </View>
+    );
+  }
