3b7a0834570d51ee1ecd930af96b2f18d3eba18d
GeunTae KIM <60951051+Rootstar07@users.noreply.github.com>
2020-12-25 20:02:07 +0900

    christmas!
    배열안의 오브젝트를 map 작업 성공

--- App.js
+++ App.js
@@ -4,21 +4,20 @@ import { ScrollView, StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   const [node, setNode] = useState(0);
-  const [toptext, setTopText] = useState("Cien años de soledad");
+  const [toptext, setTopText] = useState("제목");
   const [downtext, setDownText] = useState("내용");
+  const [index, setIndex] = useState(0);
+  const [buttonindex, SetButtonIndex] = useState(0); //실시간으로 바뀌는 것들은 모두 useState 사용
 
-  const textNode = TextNodes[0]; //textNode는 TextNodes에서 배열 위치를 가져왔어요
-
-  var text = textNode.text;
-  var Node_ID = 0;
+  //var Node_ID = 0;
 
   const onSetNode = () => {
-    const Node_ID = textNode.options[0].nextID; //Node_ID를 json의 next로 변경 -> 숫자로 하려면 Number(obj)
-    const nextTextNode = TextNodes[Node_ID];
+    setIndex(TextNodes[index].buttonindex[0]); //수정필요 -> 버튼의 인덱스를 받아 수정
+    // const Node_ID = textNode.options[0].nextID; //Node_ID를 json의 next로 변경 -> 숫자로 하려면 Number(obj)
 
-    setNode(Node_ID);
-    setTopText(nextTextNode.title);
-    setDownText(nextTextNode.text);
+    setNode(index);
+    setTopText(TextNodes[index].title);
+    setDownText(TextNodes[index].text);
   };
 
   //버튼 생성기 ver.map
@@ -27,12 +26,18 @@ export default function App() {
   //<Button title={name2} key={names.id} onPress={onSetNode}></Button>
   //)); //리스트를 받아 버튼 생성
 
-  const buttonList = TextNodes[0].button.map((name) => (
+  const buttonList = TextNodes[0].options.map((
+    name //name이 TextNodes[0].options가 되는 기적!
+  ) => (
     <Button
-      title={name}
-      key={TextNodes[0].button.id}
+      title={name.text} //배열 안의 오브젝트라도 손쉽게 다룰수있게 되었다.
+      //key 지정
       onPress={onSetNode}
     ></Button>
+  ));
+
+  const buttonindexList = TextNodes[index].buttonindex.map((buttonindex) => (
+    <Text>{buttonindex}</Text>
   ));
 
   return (
@@ -45,7 +50,6 @@ export default function App() {
       <View style={styles.textbox}>
         <ScrollView>
           <Text>{downtext}</Text>
-          <Text>{Node_ID}</Text>
         </ScrollView>
       </View>
       <View style={styles.buttonbox}>{buttonList}</View>
@@ -90,22 +94,23 @@ const TextNodes = [
       "몇 년이 지나 총살을 당하게 된 순간, 아우렐리아노 부엔디아 대령은 오래전 어느 오후 아버지에게 이끌려 얼음 구경을 하러 간 일을 떠올렸다",
     options: [
       {
-        text: "111111",
+        text: "다가갑니다.",
         setState: { blueGoo: true },
         nextID: 1,
       },
       {
-        text: "222222",
+        text: "무기를 꺼냅니다.",
         setState: { blueGoo: false },
         nextID: 3,
       },
       {
-        text: "3333333",
+        text: "상황을 지켜봅니다.",
         setState: { blueGoo: false },
         nextID: 3,
       },
     ],
     button: ["다가간다", "공격한다", "상황을 지켜본다", "도망친다"],
+    buttonindex: [0, 1, 2, 3],
   },
   {
     title: "Cien años de soledad",
