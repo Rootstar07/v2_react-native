185d91b84ab3f0ceb0a11197a59d48646244e674
GeunTae KIM <60951051+Rootstar07@users.noreply.github.com>
2020-12-25 11:38:10 +0900

    class형

--- App.js
+++ App.js
@@ -1,56 +1,55 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { Component } from "react";
 import { ScrollView, StyleSheet, Text, View, Button } from "react-native";
-import startgame from "./startgame.js";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.titlebox}>
-        <Text style={styles.title}>제목</Text>
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.titlebox}>
+          <Text style={styles.title}>제목</Text>
+        </View>
+        <View style={styles.textbox}>
+          <ScrollView>
+            <Text style={styles.text}>
+              내용 PUBLIC Stack Overflow Tags Users FIND A JOB Jobs Companies
+              TEAMS What’s this? Free 30 Day Trial React native scroll Text
+              horizontal Asked 3 years, 11 months ago Active 3 years, 11 months
+              ago Viewed 2k times 0 How to create a text scrolling bar in react
+              native? Like that of news scroll. Is it possible without using
+              Animation library? android react-native share improve this
+              question follow asked Jan 22 '17 at 13:00 Ujjwal Nepal 52944
+              silver badges1111 bronze badges add a comment 1 Answer 2 If you
+              want a user-controlled scroll you have a component for this -
+              ScrollView Here's a link to the official documentation -
+              https://facebook.github.io/react-native/docs/scrollview.html If
+              you want it to scroll automatically you need you use ScrollView
+              with the Animated library or use ScrollView's scrollTo function.
+              share improve this answer follow edited Jan 22 '17 at 14:51 PUBLIC
+              Stack Overflow Tags Users FIND A JOB Jobs Companies TEAMS What’s
+              this? Free 30 Day Trial React native scroll Text horizontal Asked
+              3 years, 11 months ago Active 3 years, 11 months ago Viewed 2k
+              times 0 How to create a text scrolling bar in react native? Like
+              that of news scroll. Is it possible without using Animation
+              library? android react-native share improve this question follow
+              asked Jan 22 '17 at 13:00 Ujjwal Nepal 52944 silver badges1111
+              bronze badges add a comment 1 Answer 2 If you want a
+              user-controlled scroll you have a component for this - ScrollView
+              Here's a link to the official documentation -
+              https://facebook.github.io/react-native/docs/scrollview.html If
+              you want it to scroll automatically you need you use ScrollView
+              with the Animated library or use ScrollView's scrollTo function.
+              share improve this answer follow edited Jan 22 '17 at 14:51
+            </Text>
+          </ScrollView>
+        </View>
+        <View style={styles.buttonbox}>
+          <Button title="button1"></Button>
+          <Button title="button2"></Button>
+          <Button title="button3"></Button>
+        </View>
       </View>
-      <View style={styles.textbox}>
-        <ScrollView>
-          <Text style={styles.text}>
-            내용 PUBLIC Stack Overflow Tags Users FIND A JOB Jobs Companies
-            TEAMS What’s this? Free 30 Day Trial React native scroll Text
-            horizontal Asked 3 years, 11 months ago Active 3 years, 11 months
-            ago Viewed 2k times 0 How to create a text scrolling bar in react
-            native? Like that of news scroll. Is it possible without using
-            Animation library? android react-native share improve this question
-            follow asked Jan 22 '17 at 13:00 Ujjwal Nepal 52944 silver
-            badges1111 bronze badges add a comment 1 Answer 2 If you want a
-            user-controlled scroll you have a component for this - ScrollView
-            Here's a link to the official documentation -
-            https://facebook.github.io/react-native/docs/scrollview.html If you
-            want it to scroll automatically you need you use ScrollView with the
-            Animated library or use ScrollView's scrollTo function. share
-            improve this answer follow edited Jan 22 '17 at 14:51 PUBLIC Stack
-            Overflow Tags Users FIND A JOB Jobs Companies TEAMS What’s this?
-            Free 30 Day Trial React native scroll Text horizontal Asked 3 years,
-            11 months ago Active 3 years, 11 months ago Viewed 2k times 0 How to
-            create a text scrolling bar in react native? Like that of news
-            scroll. Is it possible without using Animation library? android
-            react-native share improve this question follow asked Jan 22 '17 at
-            13:00 Ujjwal Nepal 52944 silver badges1111 bronze badges add a
-            comment 1 Answer 2 If you want a user-controlled scroll you have a
-            component for this - ScrollView Here's a link to the official
-            documentation -
-            https://facebook.github.io/react-native/docs/scrollview.html If you
-            want it to scroll automatically you need you use ScrollView with the
-            Animated library or use ScrollView's scrollTo function. share
-            improve this answer follow edited Jan 22 '17 at 14:51
-          </Text>
-        </ScrollView>
-      </View>
-      <View style={styles.buttonbox}>
-        <Button title="button1"></Button>
-        <Button title="button2"></Button>
-        <Button title="button3"></Button>
-      </View>
-      {startgame()}
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -81,3 +80,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
   },
 });
+
+export default App;
--- GoNextNode.js
+++ GoNextNode.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function GoNextNode() {
+export default function GoNextNode(textNodeIndex) {
   return (
     <View>
       <Text>gonextnode</Text>
