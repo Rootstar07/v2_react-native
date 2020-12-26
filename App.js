60f32d8272520839728256cc4105bab1c827d03f
GeunTae KIM <60951051+Rootstar07@users.noreply.github.com>
2020-12-26 20:14:01 +0900

    update:20.12.26
    버튼을 누르면 화면과 버튼이 동시에 바뀜!

--- App.js
+++ App.js
@@ -8,16 +8,31 @@ export default function App() {
   const [downtext, setDownText] = useState("내용");
   const [index, setIndex] = useState(0);
   const [buttonindex, SetButtonIndex] = useState(0); //실시간으로 바뀌는 것들은 모두 useState 사용
+  const [buttonList2, setButtonList2] = useState([]);
 
   //var Node_ID = 0;
 
-  const onSetNode = () => {
+  const onSetNode = () => {};
+
+  const onSetButtonList = () => {
     setIndex(TextNodes[index].buttonindex[0]); //수정필요 -> 버튼의 인덱스를 받아 수정
     // const Node_ID = textNode.options[0].nextID; //Node_ID를 json의 next로 변경 -> 숫자로 하려면 Number(obj)
 
     setNode(index);
     setTopText(TextNodes[index].title);
     setDownText(TextNodes[index].text);
+
+    setButtonList2(
+      TextNodes[index].options.map((
+        name //name이 TextNodes[0].options가 되는 기적!
+      ) => (
+        <Button
+          title={name.text} //배열 안의 오브젝트라도 손쉽게 다룰수있게 되었다.
+          //key 지정
+          onPress={onSetNode}
+        ></Button>
+      ))
+    );
   };
 
   //버튼 생성기 ver.map
@@ -26,7 +41,7 @@ export default function App() {
   //<Button title={name2} key={names.id} onPress={onSetNode}></Button>
   //)); //리스트를 받아 버튼 생성
 
-  const buttonList = TextNodes[0].options.map((
+  const buttonList = TextNodes[index].options.map((
     name //name이 TextNodes[0].options가 되는 기적!
   ) => (
     <Button
@@ -52,7 +67,10 @@ export default function App() {
           <Text>{downtext}</Text>
         </ScrollView>
       </View>
-      <View style={styles.buttonbox}>{buttonList}</View>
+      <View style={styles.buttonbox}>
+        <Button title="시작" onPress={onSetButtonList}></Button>
+        <View>{buttonList2}</View>
+      </View>
     </View>
   );
 }
@@ -94,17 +112,17 @@ const TextNodes = [
       "몇 년이 지나 총살을 당하게 된 순간, 아우렐리아노 부엔디아 대령은 오래전 어느 오후 아버지에게 이끌려 얼음 구경을 하러 간 일을 떠올렸다",
     options: [
       {
-        text: "다가갑니다.",
+        text: "다가간다.",
         setState: { blueGoo: true },
         nextID: 1,
       },
       {
-        text: "무기를 꺼냅니다.",
+        text: "무기를 꺼낸다.",
         setState: { blueGoo: false },
         nextID: 3,
       },
       {
-        text: "상황을 지켜봅니다.",
+        text: "상황을 지켜본다.",
         setState: { blueGoo: false },
         nextID: 3,
       },
