8f3a69760300d7ce237bdd9d322d3b0669eb2682
GeunTae KIM <60951051+Rootstar07@users.noreply.github.com>
2020-12-27 23:06:43 +0900

    nice
    기본틀 완성!
    i'm prod of myself

--- App.js
+++ App.js
@@ -7,11 +7,12 @@ export default function App() {
   const [toptext, setTopText] = useState("제목");
   const [downtext, setDownText] = useState("내용");
   const [index, setIndex] = useState(0);
-  const [testnum, settestnum] = useState(0);
+  const [PressedButtonID, setPressedButtonID] = useState(0);
+  const [nextButtonID, setNextButtonID] = useState(0);
 
   //var Node_ID = 0;
 
-  const onSetPage = () => {
+  const onSetPage = (nextID) => {
     //setIndex(TextNodes[index].buttonindex[0]); //수정필요 -> 버튼의 인덱스를 받아 수정
     // const Node_ID = textNode.options[0].nextID; //Node_ID를 json의 next로 변경 -> 숫자로 하려면 Number(obj)
 
@@ -28,13 +29,46 @@ export default function App() {
             color="grey"
             key={name.buttonID}
             title={name.text}
-            onPress={() => settestnum(name.buttonID)} //배열 안의 오브젝트라도 손쉽게 다룰수있게 되었다.
+            onPress={() => NeXtNode(name.nextID, name.buttonID)} //배열 안의 오브젝트라도 손쉽게 다룰수있게 되었다.
           ></Button>
         </View>
       ))
-      //12.26 버튼이 문제가 아니라 settestnum(여기)가 문제였다. 왜일까?
+      //12.26 버튼이 문제가 아니라 SetPressedButtonID(여기)가 문제였다. 왜일까?
       //목표: 클릭한 버튼 ID값 찾아내기!
     );
+  };
+
+  const onSetPage2 = (a) => {
+    //setIndex(TextNodes[index].buttonindex[0]); //수정필요 -> 버튼의 인덱스를 받아 수정
+    // const Node_ID = textNode.options[0].nextID; //Node_ID를 json의 next로 변경 -> 숫자로 하려면 Number(obj)
+
+    setNode(a); //현재는 초기값 : 0
+    setTopText(TextNodes[a].title);
+    setDownText(TextNodes[a].text);
+
+    SetButtonList2(
+      TextNodes[a].options.map((
+        name //name이 TextNodes[0].options가 되는 기적!
+      ) => (
+        <View>
+          <Button
+            color="grey"
+            key={name.buttonID}
+            title={name.text}
+            onPress={() => NeXtNode(name.nextID, name.buttonID)} //배열 안의 오브젝트라도 손쉽게 다룰수있게 되었다.
+          ></Button>
+        </View>
+      ))
+      //12.26 버튼이 문제가 아니라 SetPressedButtonID(여기)가 문제였다. 왜일까?
+      //목표: 클릭한 버튼 ID값 찾아내기!
+    );
+  };
+
+  const NeXtNode = (nextID, nowID) => {
+    setPressedButtonID(nowID);
+    setNextButtonID(nextID);
+    setIndex(nextID);
+    onSetPage2(nextID);
   };
 
   const [buttonList2, SetButtonList2] = useState([
@@ -67,7 +101,8 @@ export default function App() {
         <Text style={styles.title}>{toptext}</Text>
       </View>
       <Text>현재 인덱스 : {index}</Text>
-      <Text>누른 버튼 ID : {testnum}</Text>
+      <Text>누른 버튼 ID : {PressedButtonID}</Text>
+      <Text>다음 버튼 ID : {nextButtonID}</Text>
       <View style={styles.textbox}>
         <ScrollView>
           <Text>{downtext}</Text>
@@ -144,7 +179,7 @@ const TextNodes = [
     title: "Cien años de soledad",
     id: 1,
     text:
-      "몇 년이 지나 총살을 당하게 된 순간, 아우렐리아노 부엔디아 대령은 오래전 어느 오후 아버지에게 이끌려 얼음 구경을 하러 간 일을 떠올렸다",
+      "조물주가 창조한 모든 것은 선하나, 인간의 손 안에서 모든 것은 타락한다.",
     options: [
       {
         text: "111111",
